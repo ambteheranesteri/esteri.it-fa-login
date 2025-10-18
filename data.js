7395f4ec7142bf8f8eebb4bbd9f4791a96bfe808
ambteheranesteri <ambteheran.esteri.it@tuta.io>
2025-10-19 01:03:09 +0330

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js
+const usersData = [
+  {
+    barcode: "12345678",
+    dob: "12/05/1998",
+    captcha: "123227", // همان عددی که در HTML فعلاً نمایش داده شده
+    redirect: "success.html"
+  },
+  {
+    barcode: "87654321",
+    dob: "05/11/2002",
+    captcha: "123227",
+    redirect: "another.html"
+  }
+];
